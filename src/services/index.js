5a62fd417ef203abfb8d11fc4d9de610de8e9252
Vikthor-dev <57874994+Vikthor-dev@users.noreply.github.com>
2020-04-23 20:34:02 +0200

    Add files via upload

--- src/services/index.js
+++ src/services/index.js
@@ -6,6 +6,24 @@ let Service = axios.create({
 })
 
 let Posts = {
+   async update_post(data,id){
+       console.log("Spremam na backend: ",data);
+       let serverData={
+        title:data.title,
+        url:data.url,
+        postedBy:data.postedBy,
+        score:data.score,
+        description:data.description,
+        beach_type:data.beach_type,
+        lf_tower:data.lf_tower,
+        pets_allowed_answer:data.pets_allowed_answer,
+        free_beach:data.free_beach,
+        posted_at:data.posted_at
+       }
+       await Service.patch(`/posts/${id}`,serverData)
+       return
+
+   },
    async new_post(data){
 
     console.log("Spremam na backend:",data)
@@ -24,17 +42,34 @@ let Posts = {
     }
     await Service.post('/posts',serverData)
     return
+  },
+  async getOne(id){
+      let response = await Service.get(`/posts/${id}`)
+      let doc = response.data
+      return{
+        id:doc._id,
+        title:doc.title,
+        url:doc.url,
+        postedBy:doc.postedBy,
+        score:parseInt(doc.score),
+        description:doc.description,
+        beach_type:doc.beach_type,
+        lf_tower:doc.lf_tower,
+        pets_allowed_answer:doc.pets_allowed_answer,
+        free_beach:doc.free_beach,
+        posted_at:doc.posted_at    
+      }
   }, 
   async getAll(searchTerm){
-        let response = await Service.get(`/posts?title=${searchTerm}`)
+        let response = await Service.get(`/posts?_any=${searchTerm}`)
         let data = response.data                                      
         data = data.map(doc => {                                      
             return{
-                id:doc.id,
+                id:doc._id,
                 title:doc.title,
                 url:doc.url,
                 postedBy:doc.postedBy,
-                score:doc.score,
+                score:parseInt(doc.score),
                 description:doc.description,
                 beach_type:doc.beach_type,
                 lf_tower:doc.lf_tower,
